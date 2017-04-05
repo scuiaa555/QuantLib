c9fb9e4e51d260b7bf6776e3b7103da17d6be40c
CUI Shidong <cuishidong555@gmail.com>
2017-04-05 10:31:21 +0800

    Modified on Wed Apr  5 10:31:21 HKT 2017

---
+++ Pebbles/LiborCurve/LiborCurve.cpp
@@ -0,0 +1,22 @@
+//
+// Created by CUI Shidong on 5/4/2017.
+//
+
+#include <ql/quantlib.hpp>
+
+using namespace QuantLib;
+#include <boost/timer.hpp>
+#include <iostream>
+
+int main(int, char* []) {
+
+    boost::timer timer;
+    std::cout << std::endl;
+
+    /*********************
+     ***  MARKET DATA  ***
+     *********************/
+
+    Calendar calendar = TARGET();
+
+}
